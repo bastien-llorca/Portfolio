ec40ea7c960601703c831858d27a1526f9d1e43c
bastien-llorca <bastien.llorca@laplateforme.io>
2024-12-03 10:18:17 +0100

    dzq

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base: "/Portfolio/", // Chemin correspondant au nom du dépôt
-  site: "https://bastien-llorca.github.io/Portfolio",
+  base: "/Portfolio/dist", // Chemin correspondant au nom du dépôt
+  site: "https://bastien-llorca.github.io",
 });
