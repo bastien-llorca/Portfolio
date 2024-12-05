24002cbcc49b2e1f9d381685db6f1a6fa746339e
bastien-llorca <bastien.llorca@laplateforme.io>
2024-12-05 16:09:27 +0100

    Test build & deploy with img

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://github.com/bastien-llorca/Portfolio",
   base: "/Portfolio/", // Nom du dépôt (exemple)
 });
